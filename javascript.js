8df81a44ac5a2fb2a41f4d9b590c4cf8d650a41e
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-01-06 13:31:02 +0800

    Add functions for computer choice
    Adding the logic for what the computer will choose  in random selection than having it fixed

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
 "use strict";
+
+function randomIntFrom(min = 0, max = 3) {
+  return Math.floor(Math.random() * (max - min + min) + min);
+}
+function getComputerChoice() {
+  const choice = ["scissors", "rock", "paper"];
+  const compChoice = choice[randomIntFrom()];
+  return compChoice;
+}
+
+console.log(getComputerChoice());
