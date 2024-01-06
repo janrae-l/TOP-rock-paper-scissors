4c223c5cd466db29b430e75046da865cdcd9ae00
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-01-06 13:40:50 +0800

    Edit casing and add function for player
    Edit the casing of the computer choice array and made a function for the player's choice

--- javascript.js
+++ javascript.js
@@ -4,9 +4,20 @@ function randomIntFrom(min = 0, max = 3) {
   return Math.floor(Math.random() * (max - min + min) + min);
 }
 function getComputerChoice() {
-  const choice = ["Scissors", "Rock", "Paper"];
+  const choice = ["scissors", "rock", "paper"];
   const compChoice = choice[randomIntFrom()];
   return compChoice;
 }
 
 console.log(getComputerChoice());
+
+function playerSelection() {
+  const playerChoice = prompt(
+    "Choose between rock, paper, scissors"
+  ).toLowerCase();
+
+  return playerChoice;
+}
+
+const playerMove = playerSelection();
+console.log(playerMove);
