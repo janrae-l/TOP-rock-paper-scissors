32b2a3f5a71174c727d54e956595f76d44c9374f
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-01-08 14:10:30 +0800

    Add, edit and test the game function
    Added the best of five logic into the code and tested every single variation of the code so it works properly.

--- javascript.js
+++ javascript.js
@@ -11,13 +11,13 @@ function getComputerChoice() {
 
 //console.log(getComputerChoice());
 
-// function playerPrompt() {
-//   const playerChoice = prompt(
-//     "Choose between rock, paper, scissors"
-//   ).toLowerCase();
+function playerPrompt() {
+  const playerChoice = prompt(
+    "Choose between rock, paper, scissors"
+  ).toLowerCase();
 
-//   return playerChoice;
-// }
+  return playerChoice;
+}
 
 //const playerMove = playerPrompt();
 //console.log(playerMove);
@@ -30,23 +30,66 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === "rock" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "scissors")
   ) {
-    return (decision = `You lose! ${computerSelection} beats ${playerSelection}`);
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    decision = `You lose! ${computerSelection} beats ${playerSelection}`;
   } else if (
     (playerSelection === "scissors" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "paper") ||
     (playerSelection === "rock" && computerSelection === "rock")
   ) {
-    return decision;
+    console.log(`It's a TIE! Play again`);
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    return (decision = `You win! You beat ${computerSelection} with ${playerSelection}`);
+    console.log(
+      `You win! You beat ${computerSelection} with ${playerSelection}`
+    );
+    decision = `You win! You beat ${computerSelection} with ${playerSelection}`;
+  }
+  return decision;
+  // }
+  // return `${userScore > compScore ? userScore : compScore}`
+}
+
+//console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  let compScore = 0;
+  let userScore = 0;
+  let tieScore = 0;
+  //let decide = result.includes("lose");
+  //
+
+  for (let i = 0; i < 5; i++) {
+    let player = playerPrompt();
+    //console.log(player);
+
+    let computerOpt = getComputerChoice();
+    //console.log(computerOpt);
+
+    let result = playRound(player, computerOpt);
+
+    if (result.includes("lose")) {
+      compScore++;
+    } else if (result.includes("win")) {
+      userScore++;
+    } else if (result.includes("TIE")) {
+      tieScore++;
+    }
+    // result = playRound(playerSelection, computerSelection);
+  }
+  if (tieScore > compScore + userScore) {
+    console.log(`There are ${tieScore} tie scores. No one won the game`);
+  } else {
+    const gameResult = compScore > userScore ? "Computer" : "User";
+    console.log(
+      `${gameResult} won the game with ${
+        compScore > userScore ? compScore : userScore
+      } scores and there ${tieScore >= 2 ? "are" : "is"} ${tieScore} tie score`
+    );
   }
 }
 
-const playerSelection = "scissors";
-const computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
+game();
